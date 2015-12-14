9c33ea91644e75cda46b11c939b858d1ee3b7442
Damien Duboeuf <smeagolworms4@gmail.com>
2015-12-14 18:01:58 +0100

    Fix loading config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if (typeof global.GollumJS == 'undefined' || typeof global.GollumJS.__init == 'undefined') {
 
-var GollumJS = typeof GollumJS != 'undefined' ? GollumJS : {};
+var GollumJS = typeof (typeof window !== 'undefined' ? window : global).GollumJS != 'undefined' ? (typeof window !== 'undefined' ? window : global).GollumJS : {};
 GollumJS.__init__ = true;
 GollumJS.__running__ = "GollumJS_"+new Date().getTime()+"_"+parseInt(Math.random()*100000, 10);
 
--- src/namespace.js
+++ src/namespace.js
@@ -1,3 +1,3 @@
-var GollumJS = typeof GollumJS != 'undefined' ? GollumJS : {};
+var GollumJS = typeof (typeof window !== 'undefined' ? window : global).GollumJS != 'undefined' ? (typeof window !== 'undefined' ? window : global).GollumJS : {};
 GollumJS.__init__ = true;
 GollumJS.__running__ = "GollumJS_"+new Date().getTime()+"_"+parseInt(Math.random()*100000, 10);
