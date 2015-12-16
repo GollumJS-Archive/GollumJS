1bbca8caae2e38b2da90cbb39da72a0663381f9c
Damien Duboeuf <smeagolworms4@gmail.com>
2015-12-16 11:58:33 +0100

    Change namespace function to NS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var GollumJS = typeof (typeof window !== 'undefined' ? window : global).GollumJS
 GollumJS.__init__ = true;
 GollumJS.__running__ = "GollumJS_"+new Date().getTime()+"_"+parseInt(Math.random()*100000, 10);
 
-GollumJS.namespace = function (_ns_, cb) {
+GollumJS.NS = function (_ns_, cb) {
 	_ns_ = _ns_ !== null ? _ns_ : GollumJS.Utils.global();
 	cb.call(_ns_, _ns_);
 };
