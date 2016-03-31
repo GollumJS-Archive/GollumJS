0496e5aa31e45299d7d965410806d1cec0d83096
Damien Duboeuf <dduboeuf@dduboeuf0086>
2016-03-31 14:43:29 +0200

    Update build information

--- bin/build.js
+++ bin/build.js
@@ -7,39 +7,44 @@ fs = require('fs');
 fs.readFile(__dirname+"/../include.json", "utf8", function(err, data) {
 	if (err) throw err;
 	
-	var json = JSON.parse (data);
+	var files = JSON.parse (data);
+	
+	fs.readFile(__dirname+"/../package.json", "utf8", function(err, data) {
+		if (err) throw err;
+		
+		var package = JSON.parse (data);
+		var content = "";
+		
+		for (var i = 0; i < files.length; i++) {
 
-	var content = "";
 
-	for (var i = 0; i < json.length; i++) {
-
-
-		var buf = fs.readFileSync(__dirname+"/../"+json[i], "utf8");
-
-		content += buf+"\n\n";
-	}
-
-	fs.writeFile(__dirname+"/../build/gollumjs.js", content, function(err) {
-		if(err) {
-			return console.error(err);
+			var buf = fs.readFileSync(__dirname+"/../"+files[i], "utf8");
+			
+			content += buf+"\n\n";
 		}
-		console.log (__dirname+"/../build/gollumjs.js => OK");
-	});
-	fs.writeFile(__dirname+"/../build/gollumjs-min.js", content, function(err) {
-		if(err) {
-			return console.error(err);
-		}
-		console.log (__dirname+"/../build/gollumjs-min.js => OK");
-	});
-	fs.writeFile(
-		__dirname+"/../index.js", 
-		"if (typeof global.GollumJS == 'undefined' || typeof global.GollumJS.__init == 'undefined') {\n\n"+
-		content+
-		"\nglobal.GollumJS = GollumJS; \n} module.exports = global.GollumJS; ",
-		function(err) {
-		if(err) {
-			return console.error(err);
-		}
-		console.log (__dirname+"/../index.js => OK");
+
+		fs.writeFile(__dirname+"/../build/"+package.name+".js", content, function(err) {
+			if(err) {
+				return console.error(err);
+			}
+			console.log (__dirname+"/../build/"+package.name+".js"+" => OK");
+		});
+		fs.writeFile(__dirname+"/../build/"+package.name+"-min.js", content, function(err) {
+			if(err) {
+				return console.error(err);
+			}
+			console.log (__dirname+"/../build/"+package.name+"-min.js"+" => OK");
+		});
+		fs.writeFile(
+			__dirname+"/../index.js", 
+			"if (typeof global.GollumJS == 'undefined' || typeof global.GollumJS.__init == 'undefined') {\n\n"+
+			content+
+			"\nglobal.GollumJS = GollumJS; \n} module.exports = global.GollumJS; ",
+			function(err) {
+			if(err) {
+				return console.error(err);
+			}
+			console.log (__dirname+"/../index.js => OK");
+		});
 	});
 });
